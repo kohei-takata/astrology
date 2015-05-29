a185ed3f8c1059389c01aac7040fea1f7c628d8b
Kohei TAKATA <kt.koheitakata@gmail.com>
2015-05-29 13:27:43 +0900

    print err if argument is not passed

--- astrology.js
+++ astrology.js
@@ -4,18 +4,20 @@ var github = require('octonode');
 var client = github.client(process.argv[2]);
 var ghme = client.me();
 var ghsearch = client.search();
-
 var repository = require('./repository.json');
 
-for(var i = 0; i < repository.length; i++) {
-  ghsearch.repos({
+if(process.argv.length < 3) {
+  console.log("Please pass access token in argument");
+} else {
+  for(var i = 0; i < repository.length; i++) {
+    ghsearch.repos({
 	    q: 'repo:' + repository[i],
 	    sort: 'created',
 	    order: 'asc'
-  }, function(err, data) {
-    if(data){
-      ghme.star(data.items[0].full_name);
-    }
-  });
+    }, function(err, data) {
+      if(data){
+        ghme.star(data.items[0].full_name);
+      }
+    });
+  }
 }
-
